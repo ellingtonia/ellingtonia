f9e78dbb6ef255633affec4eb8ccbe0556eca3df
Charlie Dyson <charlie@charliedyson.net>
2022-05-24 13:40:55 +0100

    Fix: add trailing slash

--- deploy_live.sh
+++ deploy_live.sh
@@ -2,7 +2,8 @@
 set -ue -o pipefail
 
 rm -rf build
-hugo -d build -b http://ellingtonia.com
+# Trailing slash is important
+hugo -d build -b http://ellingtonia.com/
 pushd build
 
 lftp -c "open -u ellingtonia ellingtonia.com; mirror --reverse --delete"
