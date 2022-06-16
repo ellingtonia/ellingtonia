5e5b024a6a530119b382d17eefd0c2a0c1aad876
Charlie Dyson <charlie@charliedyson.net>
2022-06-16 11:40:10 +0100

    Discogs for Ai 4

--- deploy_live.sh
+++ deploy_live.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -ue -o pipefail
 
-rm -rf build
+build_dir=build_tmp
+rm -rf $build_dir
 # Trailing slash is important
-hugo -d build -b http://ellingtonia.com/
-pushd build
+hugo -d $build_dir -b http://ellingtonia.com/
+pushd $build_dir
 
 lftp -c "open -u ellingtonia ellingtonia.com; mirror --reverse --delete"
