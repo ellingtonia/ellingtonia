ec166df514ee550c0fbcf192c62ec263a8f7a9f0
Charlie Dyson <charlie@charliedyson.net>
2022-06-28 17:17:32 +0100

    Fixup directory deletion

--- deploy_live.sh
+++ deploy_live.sh
@@ -5,7 +5,9 @@ build_dir=build_tmp
 rm -rf $build_dir
 # Trailing slash is important
 hugo -d $build_dir -b http://ellingtonia.com/
-pushd $build_dir
 
+pushd $build_dir
 lftp -c "set ftp:use-mlsd on; open -u ellingtonia ellingtonia.com; mirror --reverse --delete"
+popd
+
 rm -rf $build_dir
