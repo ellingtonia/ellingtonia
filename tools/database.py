2dc1b9669b67f9258dcde50b075b1027b7244e2f
Charlie Dyson <charlie@charliedyson.net>
2025-06-02 13:48:36 +0100

    labels/releases

--- tools/database.py
+++ tools/database.py
@@ -506,10 +506,11 @@ def save_releases_to_json(database, generated):
     for release in releases:
         entries = database.entry_releases_from_release(release)
         if not entries:
+            if generated: continue
             if not generated:
                 # Only warn once (in the non-generated stage)
                 logging.warning(f"Empty release {release}; discarding")
-            continue
+                # TODO: Resume purging empty manual releases with "continue"
 
         entries.sort(key=entry_release_sort_key)
 
